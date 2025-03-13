54a50127c7d67b71c71f745520b43a5686e38b8b
ronpaul_laptop <ronakpaulpc@yahoo.com>
2025-03-14 01:22:31 +0530

    250314 first commit

---
+++ s1_introduction.R
@@ -0,0 +1,16 @@
+# Here we have practice code from the book Tidy Modelling with R by Max Kuhn 
+# and co.
+# Here we have script files for each book section.
+# Then in each script file we have code sections for each chapter.
+# This script file contains code from "Introduction" section.
+
+# Let's go!
+
+
+
+#_====
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# C0 - Installing and loading ---------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
