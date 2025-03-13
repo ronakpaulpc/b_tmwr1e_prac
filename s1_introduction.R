44e21cfab3ed6f8ab3ec837c9e00bc71c7adb30b
ronpaul_laptop <ronakpaulpc@yahoo.com>
2025-03-14 01:31:08 +0530

    mincom 240314 routine analysis

--- s1_introduction.R
+++ s1_introduction.R
@@ -12,5 +12,70 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # C0 - Installing and loading ---------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Working directory check
+getwd()
+# "C:/Users/Ronak/Documents/ALL Research/Rsoftware/b_tmwr/tmwr1e_prac"
+
+# # First we install the required packages
+# install.packages("tidymodels")
+
+# Loading the required packages
+library(easypackages)
+libraries(
+    
+)
+
+
+
+#_====
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# C1 - Software for modelling ---------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# NO CODE.
+
+
+
+#_====
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# C2 - A tidyverse primer -------------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Here, we briefly discuss important principles of the tidyverse design 
+# philosophy and how they apply in the context of modelling software 
+# that is easy to use properly and supports good statistical practice.
+
+
+# 2.1 Tidyverse principles ------------------------------------------------
+# Here we briefly describe several of the general tidyverse design principles
+# their motivation, and how we think about modelling as an application of 
+# these principles.
+
+# 2.1.1 Design for humans ====
+mtcars
+mtcars[order(mtcars$gear, mtcars$mpg), ]
+
+
+# TBC ####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
